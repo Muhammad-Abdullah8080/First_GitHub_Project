f2ca1cc66382e7b90d8dee42d52e2187df11d53f
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2021-12-03 20:25:02 +0500

    b

--- src/First_File.java
+++ src/First_File.java
@@ -1,12 +1,13 @@
 
 public class First_File {
 public static void main(String[] args) {
-	String myName="Muhammad Abdullah";
-	String rollno="20i-0687";
-	String myGrades = "A";
-	System.out.println("Name: "+myName);
+
+	String name = "Faizan";
+	String rollno = "20i-0963";
 	System.out.println("Roll No: "+rollno);
-	System.out.println("Hello");
-	
+	System.out.println("Hello World");
+
+
+
 }
 }
