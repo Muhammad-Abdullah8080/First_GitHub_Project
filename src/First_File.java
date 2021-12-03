4babf1bdaae877e8ca5ff4cb0efd253e25274150
Muhammad Abdullah <writetomuhammadabdullah@gmail.com>
2021-12-03 15:26:06 +0500

    my first commit

---
+++ src/First_File.java
@@ -0,0 +1,10 @@
+
+public class First_File {
+public static void main(String[] args) {
+	String name="Muhammad Abdullah";
+	String rollno="20i-0687";
+	System.out.println("Name: "+name);
+	System.out.println("Roll No: "+rollno);
+	
+}
+}
