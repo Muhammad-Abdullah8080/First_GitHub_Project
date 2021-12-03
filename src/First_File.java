0b0402a0306568a5ba178323cc774b10c723a652
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2021-12-03 20:21:46 +0500

    changed name

--- src/First_File.java
+++ src/First_File.java
@@ -1,7 +1,7 @@
 
 public class First_File {
 public static void main(String[] args) {
-	String name="Muhammad Abdullah";
+	String myName="Muhammad Abdullah";
 	String rollno="20i-0687";
 	System.out.println("Name: "+name);
 	System.out.println("Roll No: "+rollno);
