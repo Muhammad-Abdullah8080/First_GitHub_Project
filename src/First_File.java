f743939d0dc21fd30e220add1ee89bbbd51c4ebb
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2021-12-03 20:27:03 +0500

    c

--- src/First_File.java
+++ src/First_File.java
@@ -2,12 +2,13 @@
 public class First_File {
 public static void main(String[] args) {
 
-	String name = "Faizan";
-	String rollno = "20i-0963";
-	System.out.println("Roll No: "+rollno);
-	System.out.println("Hello World");
 
+	int num1 = 1;
+	int num2 = 3;
 
+	int addNum = num1 + num2;
+
+	System.out.println(addNum);
 
 }
 }
